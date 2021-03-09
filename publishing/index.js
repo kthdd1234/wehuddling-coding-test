e07698f3f406af2fd8a4de58d485d487cd7256cd
kthdd1234 <kthdd12@daum.net>
2021-03-09 21:11:23 +0900

    UPDATE nav-bar publishing

--- publishing/index.js
+++ publishing/index.js
@@ -1,22 +1,16 @@
 const navItems = document.querySelectorAll('.nav-item');
-const highlight = '#60a9ed';
 
 const setFirstNavItem = () => {
-  navItems[0].style.color = highlight;
-  navItems[0].style.borderBottom = `2px solid ${highlight}`;
+  navItems[0].classList.add('highlight');
 };
 
 const handleNavItemChange = (event) => {
   let target = event.target;
 
   navItems.forEach((item) => {
-    if (item === target) {
-      item.style.color = highlight;
-      item.style.borderBottom = `2px solid ${highlight}`;
-    } else {
-      item.style.color = 'rgb(80, 104, 125)';
-      item.style.borderBottom = `none`;
-    }
+    item === target
+      ? item.classList.add('highlight')
+      : item.classList.remove('highlight');
   });
 };
 
