b2c7cf7b75cc3b106dc2cdebd1b551c1dffc3618
kthdd1234 <kthdd12@daum.net>
2021-03-11 02:10:22 +0900

    Compose the app by organizing it by screen UI element

--- apps/twitter-cloning/App.js
+++ apps/twitter-cloning/App.js
@@ -4,6 +4,7 @@ import Header from './src/header';
 import Section from './src/section';
 import Footer from './src/footer';
 
+/* 화면 UI 요소별로 컴포넌트화 하여 앱을 구성 */
 const App = () => {
   const [data, setData] = useState(['']);
 
